325a7ba75933e8dd14e71331533d04c228e8645b
Velina Rusjakova <velina.rusjakova@gmail.com>
2018-11-23 01:41:30 +0100

    Integration test

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
-  get 'static_pages/help'
+  get  '/help', to: 'static_pages#help'
 
-  get 'static_pages/about'
+  get  '/about', to: 'static_pages#about'
 
-  get 'static_pages/contact'
+  get  '/contact', to: 'static_pages#contact'
 
-  root 'application#home'
+  root 'static_pages#home'
 end
