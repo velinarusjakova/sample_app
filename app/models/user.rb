48bccad17f5bcd986a8fc71a351d0c78834f98e4
Velina Rusjakova <velina.rusjakova@gmail.com>
2018-12-17 00:21:18 +0100

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,17 @@
 class User < ApplicationRecord
-	attr_accessor :remember_token
-	before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 }, 
 	format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-	validates :password, presence: true, length: { minimum: 6 }
+	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
 	has_secure_password
 
 
-	def self.digest(string)
+	  def self.digest(string)
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
       BCrypt::Password.create(string, cost: cost)
@@ -32,5 +33,18 @@ class User < ApplicationRecord
 
     def forget
       update_attribute(:remember_digest, nil)
+    end
+
+    private
+
+        # Converts email to all lower-case.
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    # Creates and assigns the activation token and digest.
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
-
-  get 'users/new'
-  get  '/help', to: 'static_pages#help'
-  get  '/about', to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
-  get  '/signup', to: 'users#new'
-  post '/signup',to: 'users#create'
-  root 'static_pages#home'
+  root   'static_pages#home'
+  get    '/help',    to: 'static_pages#help'
+  get    '/about',   to: 'static_pages#about'
+  get    '/contact', to: 'static_pages#contact'
+  get    '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
+  post   '/signup',  to: 'users#create'
   delete '/logout',  to: 'sessions#destroy'
-
   resources :users
+  resources :account_activations, only: [:edit]
 end
