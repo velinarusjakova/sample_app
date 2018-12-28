8f22b86601bdf2b6dbf66e2d18ccddfc4ac90173
Velina Rusjakova <velina.rusjakova@gmail.com>
2018-12-28 16:34:34 +0100

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
   	 @user = User.find(params[:id])
+     @microposts = @user.microposts.paginate(page: params[:page])
      redirect_to root_url and return unless @user.activated?
   end
 
@@ -25,6 +26,7 @@ class UsersController < ApplicationController
 
   def edit
   end
+
 
 
   def update
@@ -52,14 +54,6 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
-    # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
 
     def correct_user
       @user = User.find(params[:id])
